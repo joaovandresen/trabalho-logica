078db5eaff3670313c73e0c7bebb2cc974df6e5b
João Victor <joao.vandresen.costa@escola.pr.gov.br>
2025-11-13 12:03:10 +0000

    ok

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
 const lampada = document.getElementById("lampada");
 
-opcao1.addEventListener("mouseover",opcao1);
-opcao2.addEventListener("mouseout",opcao2);
-opcao3.addEventListener("mouseover",opcao3);
-opcao4.addEventListener("mouseout", opcao4);
+computador.addEventListener("mouseover",ligar);
+computador.addEventListener("mouseout",desligar);
 
-function opcao1(){
+function acender(){
     lampada.src = "on.png";
-    document.body.style.background = "#b1b1b1ff"
+    document.body.style.background = "#ffeb3b22"
 }
-function opcao2(){
+function apagar(){
     lampada.src = "off.png";
-    document.body.style.background = "#b1b1b1ff"
+    document.body.style.background = "#222"
     
 }
-function opcao3(){
-    opcao3.src = "imagem3.png"
-    document.body.style.background = "#a5a5a5ff"
-}
-function opcao4(){
-    opcao4.src = "imagem4.png"
-    document.body.style.background = "#979797ff"
-}
-function opcao5(){
-    opcao5.src = "imagem5.png"
-    document.body.style.background = "#cfcfcfff"
-}
-
