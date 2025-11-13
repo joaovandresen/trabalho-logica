a5404f25aed4eb9762d5b95dafa6b0a9c7201bc2
João Victor <joao.vandresen.costa@escola.pr.gov.br>
2025-11-13 12:16:42 +0000

    código pronto

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const computador = document.getElementById("computador");
 function ligar(){
 
     computador.src = "on.jpg"; 
-    document.body.style.background = "#00000022"
+    document.body.style.background = "#00000ff"
 }
 function desligar(){
     computador.src = "off.jpg"; 
